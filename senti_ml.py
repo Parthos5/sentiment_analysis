a8df60f7ef48f7f0c4ef373b8adc8fad0d147ae7
Parth Puranik <thosparth@gmail.com>
2024-02-26 23:59:50 +0530

    update

--- senti_ml.py
+++ senti_ml.py
@@ -1,5 +1,32 @@
 import os
 import pandas as pd 
+import nltk
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def preprocess_text(text):
+    # Tokenization
+    tokens = word_tokenize(text)
+    
+    # Lowercasing
+    tokens = [word.lower() for word in tokens]  
+    # Removing punctuation
+    tokens = [word for word in tokens if word.isalnum()]
+    # Removing stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    # Stemming
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(word) for word in tokens]
+    # Join tokens back into a single string
+    preprocessed_text = ' '.join(tokens)  
+    return preprocessed_text
 
 dataset_folder = "./aclImdb"
 
@@ -16,5 +43,20 @@ def load_dataset():
     return pd.DataFrame(data)
 
 dataset = load_dataset()
-
+dataset['processef_review'] = dataset['review'].apply(preprocess_text)
 print(dataset.head())
+
+# Split the dataset into training and testing sets
+train_data, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
+
+# Print the shapes of the training and testing sets
+print("Training set shape:", train_data.shape)
+print("Testing set shape:", test_data.shape)
+
+tfidf_vectorizer = TfidfVectorizer()
+X_train_tfidf = tfidf_vectorizer.fit_transform(train_data['processed-review'])
+X_test_tfidf = tfidf_vectorizer.transform(test_data['processed-review'])
+
+print("Training set TF-IDF shape:", X_train_tfidf.shape)
+print("Testing set TF-IDF shape:", X_test_tfidf.shape)
+
